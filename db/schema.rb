0ee70be2a11435d9467bfd1af58f68e5b83dc49c
Milosz Frejnik <milosz@frejnik.com>
2015-03-22 16:46:10 +0100

    generate user model, fix some tests afterwards

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315084758) do
+ActiveRecord::Schema.define(version: 20150322133143) do
 
   create_table "todo_items", force: true do |t|
     t.integer  "todo_list_id"
@@ -29,5 +29,16 @@ ActiveRecord::Schema.define(version: 20150315084758) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_difest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end
